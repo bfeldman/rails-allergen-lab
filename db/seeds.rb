4055385da61190d6dc01f2ea78454320c2c1489d
bfeldman <brian.feldman42@gmail.com>
2020-10-12 19:41:56 -0400

    initial db setup

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+50.times {
+    User.create(name: Faker::Name.unique.name)
+}
+
+50.times {
+    Recipe.create(title: Faker::Food.unique.dish)
+}
+
+150.times {
+    Ingredient.create(name: Faker::Food.unique.ingredient)
+}
+
+Recipe.all.each do |recipe|
+    3.times {
+        RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: Ingredient.all.sample(1).id)
+    }
+end
+
+
+25.times {
+    Allergen.create(user_id: User.all.sample(1)[0].id, ingredient_id: Ingredient.all.sample(1)[0].id)
+}
