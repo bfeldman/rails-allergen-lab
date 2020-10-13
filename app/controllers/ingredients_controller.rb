6a61bcf6ac0ae34129dc44e5c1dbca729d326d2c
bfeldman <brian.feldman42@gmail.com>
2020-10-13 01:55:28 +0000

    taking a break

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,12 @@
 class IngredientsController < ApplicationController
   def index
+    @ingredients = Ingredient.all
   end
 
   def show
+    @ingredient = Ingredient.find(params[:id])
   end
 end
+
+
+# Ingredient.all.map {|ingr| Allergen.all.count{|a| a.ingredient_id == ingr.id } }
